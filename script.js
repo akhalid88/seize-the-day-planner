2e920e4ed04bcac2f3871b129f4ab91bf993dbff
Muhammad Khalid <akhalid.code@gmail.com>
2020-12-31 00:14:32 -0800

    code cleanup

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var array = [];
 // "", "", "", "", "", "", "", "", ""
 $(document).ready(function () {
 
-  var time = moment().format("HH");
-  var date = moment().format("dddd, MMMM Do");
-  //var time = moment().format("LT");
-
+  //for header tag
+	var date = moment().format("dddd, MMMM Do");
+	//for highlighting functionality
+	var time = moment().format("HH");
+	
+	//inits planner
   loadEvents();
   highlighter();
 
@@ -14,47 +16,32 @@ $(document).ready(function () {
   function highlighter() {
 
     $(".container .row").each(function () {
-	
-			// console.log("24 hour time: " + time);
 			var hour = $(this).children().first().attr("data-time");
-      var block = $(this).children().first().siblings();
-			//console.log("DEBUG: " + hour);
-			// siblings().addClass("present"));
-
+			var block = $(this).children().next(".description");
+			
       if (time == hour) {
 				block.addClass("present");
-				//console.log("Living in the moment " + time + " is " + hour);
-		 
 			} else if (time < hour) {
 				block.addClass("future");
-				//console.log("Welcome to the future " + hour + " is greater than " + time);
-			
 			} else {
 				block.addClass("past");
-				//console.log("Days gone by " + hour + " is less than " + time);
-      }
-
+			}
     })
   }
 
   function saveEvent() {
     localStorage.setItem("events", JSON.stringify(array));
-		highlighter();
-    //DEBUG
-    console.log("Events to save: " + array)
-  }
+	}
 
   function loadEvents() {
-    if (localStorage.length === 0) {
-      //DEBUG
-      console.log("events = null")
+		if (localStorage.length === 0) {
+			//Error handling; don't attempt JSON parsing
+			console.log("events = null")
     } else {
+			//Proceed with parsing and loading events
       var storedEvents = JSON.parse(localStorage.getItem("events"));
-
-      // DEBUG
-      console.log("Events to load: " + storedEvents);
-
-      //loop through all rows withing main container div
+     
+      //Loop through all rows within main container div
       $(".container .row").each(function () {
         $(this).find(".description").text(storedEvents[$(this).attr("data-index")]);
       });
@@ -65,7 +52,8 @@ $(document).ready(function () {
   }
 
   $(".container .row").on("click", function (event) {
-    event.preventDefault();
+		event.preventDefault();
+		highlighter();
     var element = event.target;
 
     //only take action if a button is pressed
@@ -75,70 +63,3 @@ $(document).ready(function () {
     }
   });
 });
-
-// console.log("Element: " + element);
-// console.log("Data-Index: " + element.parentElement.getAttribute("data-index"));
-
-
-//Functions needed
-//getTime() : gets current time
-//compareTime(time slot) : compares time with time slot
-//cellHighlighter() : highlight cells based on time
-//saveToLocal() : on save button click save associated entry to local storage
-//getFromLocal() : on load grab data from local storage
-
-
-// User Story
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
-
-// Acceptance Criteria
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-//  Requires use of date/time api; research usage
-
-
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-//  break each time slot into (time header, input field, and save button) nested within calendar
-//  option A: each time slot has its own listener
-//  option B: single listener for entire calendar and use ids to update data and ui
-
-
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-//  cellHighlighter() function
-//  compares each time slot with current time and provides the appropriate highlighting
-//  relies heavily on how the date/time api works
-
-
-// WHEN I click into a time block
-// THEN I can enter an event
-//  standard input field
-
-
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-//  standard button with additional save icon. 
-
-
-// WHEN I refresh the page
-// THEN the saved events persist
-// save to localStorage
-// JSON.stringify
-// JSON.parse
-
-
-//Unix timestamps for comparison
-1609393092216
-// 09:00 = 1609261200
-// 10:00 = 1609264800
-// 11:00 = 1609268400
-// 12:00 = 1609272000
-// 01:00 = 1609275600
-// 02:00 = 1609279200
-// 03:00 = 1609282800
-// 04:00 = 1609286400
-// 05:00 = 1609290000
