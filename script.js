09882052eb9038121135fe22c73855a1bc35a8a2
Muhammad K <akhalid.code@gmail.com>
2021-12-28 19:07:28 -0800

    fix highlighting and save to localStorage error

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
 var array = ["", "", "", "", "", "", "", "", ""];
 
 $(document).ready(function () {
-
   //for header tag
-	var date = moment().format("dddd, MMMM Do");
-	//for highlighting functionality
-	var time = moment().format("HH");
-	
-	//inits planner
+  var date = moment().format("dddd, MMMM Do");
+  //for highlighting functionality
+  var time = moment().format("HH");
+
+
+  //inits planner
   loadEvents();
   highlighter();
 
   $("#currentDay").text(date);
 
   function highlighter() {
-
     $(".container .row").each(function () {
-			var hour = $(this).children().first().attr("data-time");
-			var block = $(this).children().next(".description");
-			
+      var hour = $(this).children().first().attr("data-time");
+      var block = $(this).children().next(".description");
+
       if (time == hour) {
-				block.addClass("present");
-			} else if (time < hour) {
-				block.addClass("future");
-			} else {
-				block.addClass("past");
-			}
+        block.addClass("present");
+      } else if (time < hour) {
+        block.addClass("future");
+      } else {
+        block.addClass("past");
+      }
     })
   }
 
   function saveEvent() {
     localStorage.setItem("events", JSON.stringify(array));
-	}
+    console.log("Events saved!")
+  }
 
   function loadEvents() {
-		if (localStorage.length === 0) {
-			//Error handling; don't attempt JSON parsing if localStorage is empty
-			console.log("events = null")
+    if (localStorage.length === 0 || localStorage.getItem("events") == null) {
+      //Error handling; don't attempt JSON parsing if localStorage is empty
+      console.log("events = null")
     } else {
-			//Proceed with parsing and loading events
+      console.log("eawerraew")
+      //Proceed with parsing and loading events
       var storedEvents = JSON.parse(localStorage.getItem("events"));
-     
+
       //Loop through all rows within main container div
       $(".container .row").each(function () {
         $(this).find(".description").text(storedEvents[$(this).attr("data-index")]);
@@ -52,10 +53,11 @@ $(document).ready(function () {
   }
 
   $(".container .row").on("click", function (event) {
-		event.preventDefault();
-		highlighter();
+    event.preventDefault();
+    highlighter();
     var element = event.target;
-
+    console.log("eawerraew")
+    console.log(event.target)
     //only take action if a button is pressed
     if (element.matches("button") === true) {
       array[element.parentElement.getAttribute("data-index")] = $(element).siblings(".description").val();
